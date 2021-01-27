030e6933068301ee96db2766aa569d36b89b7735
Brian Lam <brilam8@gmail.com>
2021-01-27 11:02:15 -0800

    finish

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -2,6 +2,6 @@ var intervalID = setInterval(displayTime, 1000)
 
 function displayTime() {
     let d = new Date();
-    let time = d.toLocaleTimeString()
+    let time = d.toLocaleTimeString();
     console.log(time);
 }
